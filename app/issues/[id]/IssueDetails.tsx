0f5092af9a347190b7a5776c99539c54e26f5145
Sachin <sachinlakshan04@gmail.com>
2024-02-29 17:20:32 +0530

    Add a delete button.

--- app/issues/[id]/IssueDetails.tsx
+++ app/issues/[id]/IssueDetails.tsx
@@ -15,7 +15,7 @@ const IssueDetails = ({ issue }: Props) => {
         <IssueStatusBadge status={issue.status} />
         <Text>{issue.createdAt.toDateString()}</Text>
       </Flex>
-      <Card className="prose" mt="5">
+      <Card className="prose max-w-full" mt="5">
         <Markdown>{issue.description}</Markdown>
       </Card>
     </>
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "@radix-ui/themes/styles.css";
 import "./theme-config.css";
 import "./globals.css";
 import NavBar from "./NavBar";
-import { Theme } from "@radix-ui/themes";
+import { Container, Theme } from "@radix-ui/themes";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body className={inter.variable}>
         <Theme accentColor="violet">
           <NavBar />
-          <main className="p-5">{children}</main>
+          <main className="p-5">
+            <Container>{children}</Container>
+          </main>
         </Theme>
       </body>
     </html>
