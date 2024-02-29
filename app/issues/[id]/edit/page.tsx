24815d4c5cebfaf4986e4030d5cf128fb99bff06
Sachin <sachinlakshan04@gmail.com>
2024-02-29 15:26:51 +0530

    Improve the loading experience.

--- app/issues/[id]/edit/page.tsx
+++ app/issues/[id]/edit/page.tsx
@@ -1,6 +1,12 @@
 import { notFound } from "next/navigation";
-import IssueForm from "../../_components/IssueForm";
+import dynamic from "next/dynamic";
 import prisma from "@/prisma/client";
+import IssueFormSkeleton from "../../_components/IssueFormSkeleton";
+
+const IssueForm = dynamic(() => import("@/app/issues/_components/IssueForm"), {
+  ssr: false,
+  loading: () => <IssueFormSkeleton />,
+});
 
 interface Props {
   params: { id: string };
--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -1,23 +1,19 @@
 "use client";
 
-import { Box, Button, Callout, Text, TextField } from "@radix-ui/themes";
-import "easymde/dist/easymde.min.css";
-import dynamic from "next/dynamic";
-import { useForm, Controller } from "react-hook-form";
-import axios from "axios";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
-import { InfoCircledIcon } from "@radix-ui/react-icons";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { IssueSchema } from "@/app/validationSchemas";
-import { z } from "zod";
 import ErrorMessage from "@/app/components/ErrorMessage";
 import Spinner from "@/app/components/Spinner";
+import { IssueSchema } from "@/app/validationSchemas";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Issue } from "@prisma/client";
-
-const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
-  ssr: false,
-});
+import { InfoCircledIcon } from "@radix-ui/react-icons";
+import { Box, Button, Callout, TextField } from "@radix-ui/themes";
+import axios from "axios";
+import "easymde/dist/easymde.min.css";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import SimpleMDE from "react-simplemde-editor";
+import { z } from "zod";
 
 type IssueFormData = z.infer<typeof IssueSchema>;
 
