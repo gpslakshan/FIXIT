70fb8a90f5611118048354135993672e45f8701e
Sachin <sachinlakshan04@gmail.com>
2024-02-29 12:40:23 +0530

    Add additional loading skeltons.

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,5 +1,20 @@
+import { Box, Flex, Card } from "@radix-ui/themes";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
 const LoadingIssueDetailsPage = () => {
-  return <div>Loading...</div>;
+  return (
+    <Box className="max-w-xl">
+      <Skeleton />
+      <Flex gap="2" my="2">
+        <Skeleton width="5rem" />
+        <Skeleton width="8rem" />
+      </Flex>
+      <Card mt="5">
+        <Skeleton count={5} />
+      </Card>
+    </Box>
+  );
 };
 
 export default LoadingIssueDetailsPage;
