39d6f654244a48f9e338b0933b705d62f98699b7
Sachin <sachinlakshan04@gmail.com>
2024-03-01 00:15:39 +0530

    wip

--- app/issues/(issues)/page.tsx
+++ app/issues/(issues)/page.tsx
@@ -1,7 +1,7 @@
 import { IssueStatusBadge, Link } from "@/app/components";
 import prisma from "@/prisma/client";
 import { Box, Table } from "@radix-ui/themes";
-import IssueActions from "./(issues)/IssueActions";
+import IssueActions from "./IssueActions";
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
