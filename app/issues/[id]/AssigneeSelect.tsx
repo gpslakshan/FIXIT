d205a936c194de3f6b9dea3026f1476f6f403d7e
Sachin <sachinlakshan04@gmail.com>
2024-03-01 21:11:22 +0530

    Show toast notifications.

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -5,6 +5,7 @@ import { Select } from "@radix-ui/themes";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { Skeleton } from "@/app/components";
+import toast, { Toaster } from "react-hot-toast";
 
 interface Props {
   issue: Issue;
@@ -23,14 +24,20 @@ const AssigneeSelect = ({ issue }: Props) => {
   });
 
   const handleAssigneeSelect = async (userId: string) => {
-    if (userId === "un-assigned") {
-      await axios.patch(`/api/issues/${issue.id}`, {
-        assignedToUserId: null,
-      });
-    } else {
-      await axios.patch(`/api/issues/${issue.id}`, {
-        assignedToUserId: userId,
-      });
+    try {
+      if (userId === "un-assigned") {
+        await axios.patch(`/api/issues/${issue.id}`, {
+          assignedToUserId: null,
+        });
+        toast.success("Successfully un-assigned the issue!");
+      } else {
+        await axios.patch(`/api/issues/${issue.id}`, {
+          assignedToUserId: userId,
+        });
+        toast.success("Successfully assigned the issue!");
+      }
+    } catch (error) {
+      toast.error("Changes could not be saved!");
     }
   };
 
@@ -39,23 +46,26 @@ const AssigneeSelect = ({ issue }: Props) => {
   if (error) return null;
 
   return (
-    <Select.Root
-      defaultValue={issue.assignedToUserId || "un-assigned"}
-      onValueChange={handleAssigneeSelect}
-    >
-      <Select.Trigger placeholder="Assign..." />
-      <Select.Content>
-        <Select.Group>
-          <Select.Label>Suggestions</Select.Label>
-          <Select.Item value="un-assigned">Unassigned</Select.Item>
-          {users?.map((user) => (
-            <Select.Item key={user.id} value={user.id}>
-              {user.name}
-            </Select.Item>
-          ))}
-        </Select.Group>
-      </Select.Content>
-    </Select.Root>
+    <>
+      <Select.Root
+        defaultValue={issue.assignedToUserId || "un-assigned"}
+        onValueChange={handleAssigneeSelect}
+      >
+        <Select.Trigger placeholder="Assign..." />
+        <Select.Content>
+          <Select.Group>
+            <Select.Label>Suggestions</Select.Label>
+            <Select.Item value="un-assigned">Unassigned</Select.Item>
+            {users?.map((user) => (
+              <Select.Item key={user.id} value={user.id}>
+                {user.name}
+              </Select.Item>
+            ))}
+          </Select.Group>
+        </Select.Content>
+      </Select.Root>
+      <Toaster position="top-right" />
+    </>
   );
 };
 
