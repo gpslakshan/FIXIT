55e0baf9862365b769ec6f0fca0e43f18982acd9
Sachin <sachinlakshan04@gmail.com>
2024-03-02 17:11:39 +0530

    Sort issues.

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -22,7 +22,16 @@ const IssuesPage = async ({ searchParams }: Props) => {
     ? searchParams.status
     : undefined;
 
-  const issues = await prisma.issue.findMany({ where: { status } });
+  const orderBy = columns
+    .map((column) => column.value)
+    .includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: "asc" }
+    : undefined;
+
+  const issues = await prisma.issue.findMany({
+    where: { status },
+    orderBy,
+  });
 
   return (
     <Box>
