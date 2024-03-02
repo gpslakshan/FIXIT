13efb3e2dc017708507eb32d4274058e8a12b44c
Sachin <sachinlakshan04@gmail.com>
2024-03-02 21:25:35 +0530

    Build the layout of the paginator component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import Paginator from "./components/Paginator";
+
 export default function Home() {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <Paginator itemCount={100} pageSize={10} currentPage={10} />
+    </div>
+  );
 }
